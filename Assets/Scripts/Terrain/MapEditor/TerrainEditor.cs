b7e1640cf9bf756d86091325de0b938a759b2004
Thrax <evengerfirs@rocketmail.com>
2022-11-21 22:39:57 +0100

    Implemented brush scaling and terrain modification undo

---
+++ Assets/Scripts/Terrain/MapEditor/TerrainEditor.cs
@@ -0,0 +1,376 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainEditor : MonoBehaviour
+{
+    [SerializeField] private Terrain terrain;
+    [SerializeField] private ComputeShader brushDropoffShader;
+    [SerializeField] private float brushSize;                       // Scale of brush (default 1)                                       TODO: unserialize
+    [SerializeField] private string mapNameForLoadSave;             // Name under which the map will be saved                           TODO: unserialize
+    [SerializeField] private  Texture2D heightmapSaveLoadBuffer;    // Memory buffer used to store the map to be loaded or to be saved  TODO: unserialize
+    [SerializeField] private Texture2D originalBrush;               // Original brush loaded from file                                  TODO: unserialize
+    [SerializeField] private Texture2D brushForManipulation;        // Brush stored in memory, and manipulated(scaled)                  TODO: unserialize
+    [SerializeField] private float brushStrenght;                   //                                                                  TODO: MAYBE inherit brush strength for every pixel from brush?  TODO: unserialize
+    [SerializeField] private float brushScaleIncrement = 0.1f;      // Increment in which the brush gets scaled up
+
+
+    struct brushPixel
+    {
+        public int xPos;
+        public int yPos;
+        public float pixelBrushStrength;
+    }
+
+    private int brushLength;
+    private float[,] mesh;
+    private RaycastHit hit;
+    private Ray ray;
+    private float realBrushStrenght;
+    private brushPixel[] loadedBrush;
+    private brushPixel[] computedBrush;
+    private int hitX;
+    private int hitZ;
+    private List<float[,]> terrainUndoStack = new List<float[,]>();
+    private List<string> undoMemoryStack = new List<string>();
+    private bool terrainManipulationActive = false;
+  
+    private void Start()
+    {
+        realBrushStrenght = brushStrenght/1000;
+        mesh = new float[terrain.terrainData.heightmapResolution,terrain.terrainData.heightmapResolution];
+        for( int i = 0; i < terrain.terrainData.heightmapResolution;i++ )
+        {
+            for( int j = 0; j < terrain.terrainData.heightmapResolution;j++ )
+            {
+                mesh[i,j] = 0.4f;                                                                                   //  set base height 
+            }                                                                                                       //  TODO: make custimisable / resetable
+        }
+
+        terrainUndoStack.Add(returnCopyOfMesh(mesh));
+        undoMemoryStack.Add("terrainManipulation");
+        
+        this.terrain.terrainData.SetHeights(0,0,mesh);
+        loadBrushFromPngAndCalculateBrushPixels(true,"craterBrush");                                                // TODO: Runtime brush picker
+    }
+
+    private float[,] returnCopyOfMesh(float[,] originalMesh)
+    {
+        var returnMesh = new float[originalMesh.GetLength(0),originalMesh.GetLength(1)];
+        for( int i = 0; i < originalMesh.GetLength(0);i++ )
+        {
+            for( int j = 0; j < originalMesh.GetLength(1);j++ )
+            {
+                returnMesh[i,j] = originalMesh[i,j];                                                                                  //  set base height 
+            }                                                                                                       //  TODO: make custimisable / resetable
+        }
+
+        return returnMesh;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (terrainManipulationActive == true)
+        {
+            if(!Input.GetMouseButton(0) && !Input.GetMouseButton(1))
+            {
+                if (undoMemoryStack.Count > 50)
+                {
+                    undoMemoryStack.RemoveAt(0);
+                    terrainUndoStack.RemoveAt(0);
+                }
+                terrainUndoStack.Add(returnCopyOfMesh(mesh));
+                undoMemoryStack.Add("terrainManipulation");
+                terrainManipulationActive = false;
+            }
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            raiseOrLowerTerrain(true);
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            raiseOrLowerTerrain(false);
+        }
+
+        if(Input.GetKeyUp(KeyCode.K))                                // Temporary save key
+        {
+            saveTerrainHeightmapToFolder(mapNameForLoadSave);        // TODO place this function on GUI object
+        }
+
+        if(Input.GetKeyUp(KeyCode.L))                                // Temporary load key
+        {
+            loadTerrainfromFolder(mapNameForLoadSave);               // TODO place this function on GUI object
+        }
+
+        if(Input.GetKeyUp(KeyCode.Comma))                            // Temporary scale down key
+        {
+            if((brushSize -= brushScaleIncrement) < 4)
+            {
+                brushSize -= brushScaleIncrement;                     // TODO place this function on GUI object
+                scaleBrush();
+                loadBrushFromPngAndCalculateBrushPixels(false);
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.Period))                           // Temporary scale up key
+        {
+            if((brushSize += brushScaleIncrement) > 0)
+            {
+                brushSize += brushScaleIncrement;                     // TODO place this function on GUI object
+                scaleBrush();
+                loadBrushFromPngAndCalculateBrushPixels(false);
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.G))   // Temporary undo key       replace G with Input.GetKeyUp(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.Z)
+        {
+            undoAction();               // TODO place this function on GUI object
+        }
+    }
+
+    private void undoAction()
+    {
+        string previousAction;
+        if(undoMemoryStack.Count > 1)
+        {
+            previousAction = undoMemoryStack[undoMemoryStack.Count-1];
+            if(undoMemoryStack.Count > 1)
+            {
+                undoMemoryStack.RemoveAt(undoMemoryStack.Count-1);
+            }
+            
+            if(previousAction == "terrainManipulation")
+            {
+                undoTerrainManipulation();
+            }
+            else if (previousAction == "textureManipulation")
+            {
+                undoTextureManipulation();
+            }
+            else if (previousAction == "objectManipulation")
+            {
+                undoObjectManipulation();
+            }
+        }
+    }
+
+
+    private void undoTerrainManipulation()
+    {
+        this.terrain.terrainData.SetHeights(0,0, terrainUndoStack[terrainUndoStack.Count-2]);
+        mesh = returnCopyOfMesh(terrainUndoStack[terrainUndoStack.Count-2]);
+        terrainUndoStack.RemoveAt(terrainUndoStack.Count-1);
+  
+    }
+
+    private void undoTextureManipulation()
+    {
+        // TODO texture undo logic
+    }
+
+    private void undoObjectManipulation()
+    {
+        // TODO object undo logic
+    }
+
+
+    private void loadTerrainfromFolder(string mapName)
+    {
+        byte[] fileData;
+
+        if (System.IO.File.Exists("Assets/ExportedHeightmaps/" + mapName + ".png"))
+        {
+            fileData = System.IO.File.ReadAllBytes("Assets/ExportedHeightmaps/" + mapName + ".png");
+            heightmapSaveLoadBuffer = new Texture2D(2, 2);
+            heightmapSaveLoadBuffer.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        
+
+            mesh = new float[terrain.terrainData.heightmapResolution,terrain.terrainData.heightmapResolution];
+            for( int i = 0; i < terrain.terrainData.heightmapResolution;i++ )
+            {
+                for( int j = 0; j < terrain.terrainData.heightmapResolution;j++ )
+                {
+                    
+                    mesh[i,j] = heightmapSaveLoadBuffer.GetPixel(j,i).g;
+                }  
+            }
+            this.terrain.terrainData.SetHeights(0,0,mesh);
+        }
+        else
+        {
+            Debug.Log("Map Not Found - implement real error handling function");
+        }
+
+    }
+
+    /// <param name="mapName">The file name for the exported map</param>
+    private void saveTerrainHeightmapToFolder(string mapName)
+    {
+        heightmapSaveLoadBuffer =  new Texture2D(terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution);
+        for( int i = 0; i < terrain.terrainData.heightmapResolution;i++ )
+        {
+            for( int j = 0; j < terrain.terrainData.heightmapResolution;j++ )
+            {
+                Color pixel = new Color(mesh[j,i],mesh[j,i],mesh[j,i],1);
+                heightmapSaveLoadBuffer.SetPixel(i,j,pixel);
+            }     
+        }
+        heightmapSaveLoadBuffer.Apply();
+
+        byte[] _bytes =heightmapSaveLoadBuffer.EncodeToPNG();
+        System.IO.File.WriteAllBytes("Assets/ExportedHeightmaps/" + mapName + ".png", _bytes);
+
+    }
+    
+
+    /// <summary>
+    /// Raises or loweres terrain at the mouse position according to the brush. 
+    /// </summary>
+    /// <param name="raise">Flag that determines if terains should be lowered or raised (True = raise) (False = lower)</param>
+    private void raiseOrLowerTerrain(bool raise)
+    {
+        var modifier = 1;
+        if(!raise)
+        {
+            modifier = -1;
+        }
+
+        ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        if (Physics.Raycast (ray, out hit)) 
+        {
+            hitZ = Mathf.RoundToInt((hit.point - terrain.GetPosition()).z/terrain.terrainData.size.z * terrain.terrainData.heightmapResolution);
+            hitX = Mathf.RoundToInt((hit.point - terrain.GetPosition()).x/terrain.terrainData.size.x * terrain.terrainData.heightmapResolution);
+            realBrushStrenght = brushStrenght/1000;
+            // calculate brush strenghts with compute shader
+            brushDropoffShader.SetFloat("brushWidth",brushForManipulation.width);
+            brushDropoffShader.SetFloat("brushHeight",brushForManipulation.height);
+            ComputeBuffer buffer = new ComputeBuffer(loadedBrush.Length,sizeof(int)*2+sizeof(float));   
+            buffer.SetData(loadedBrush);
+            int kernel = brushDropoffShader.FindKernel("HardManipultionTool");
+            brushDropoffShader.SetBuffer(kernel, "loadedBrush", buffer);
+            brushDropoffShader.Dispatch(kernel,(int)Mathf.Ceil(loadedBrush.Length/64f),1,1);
+            brushLength = (int)Mathf.Ceil(loadedBrush.Length/64f);
+            buffer.GetData(computedBrush);
+
+            buffer.Dispose();
+            
+            for(int i=0; i< computedBrush.Length;i++)
+            {
+                if(hitZ+computedBrush[i].xPos > 0 && hitX+computedBrush[i].yPos > 0 && hitZ+computedBrush[i].xPos < terrain.terrainData.heightmapResolution && hitX+computedBrush[i].yPos < terrain.terrainData.heightmapResolution)
+                {
+                    
+                    if( mesh[hitZ+computedBrush[i].xPos,hitX+computedBrush[i].yPos] + computedBrush[i].pixelBrushStrength * modifier < 1  )
+                    {
+                        mesh[hitZ+computedBrush[i].xPos,hitX+computedBrush[i].yPos] += computedBrush[i].pixelBrushStrength * modifier;
+                    }
+                    else
+                    {
+                        mesh[hitZ+computedBrush[i].xPos,hitX+computedBrush[i].yPos] = 1;
+                    }
+                    if( mesh[hitZ+computedBrush[i].xPos,hitX+computedBrush[i].yPos] + computedBrush[i].pixelBrushStrength * modifier > 0 )
+                    {
+                        mesh[hitZ+computedBrush[i].xPos,hitX+computedBrush[i].yPos] += computedBrush[i].pixelBrushStrength * modifier;
+                    }
+                    else
+                    {
+                        mesh[hitZ+computedBrush[i].xPos,hitX+computedBrush[i].yPos] = 0;
+                    }
+                }
+                loadedBrush[i].pixelBrushStrength = realBrushStrenght;
+            }
+            this.terrain.terrainData.SetHeights(0,0,mesh);
+            terrainManipulationActive = true;
+        }
+    }
+
+
+    /// <summary>
+    /// Loads brush (Optional).
+    /// Calculates the position of black pixels on the loaded brush. 
+    /// </summary>
+    /// <param name="loadFromFile">Flag to indicate if it should be a new texture loaded from file or just recompute pixels for brush</param>
+    /// <param name="brushName">Name of the file in the brush folder without the file extension (file needs to be .png)</param>
+    private void loadBrushFromPngAndCalculateBrushPixels(bool loadFromFile ,string brushName = "")
+    {
+        byte[] fileData;
+        Texture2D brushForLoading = null;
+
+
+        if (System.IO.File.Exists("Assets/Brushes/" + brushName + ".png") || loadFromFile == false)
+        {
+            if(loadFromFile == true)
+            {
+                fileData = System.IO.File.ReadAllBytes("Assets/Brushes/" + brushName + ".png");
+                originalBrush = new Texture2D(2, 2);
+                originalBrush.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+                brushForLoading = originalBrush;
+                brushForManipulation = originalBrush;
+            }
+            else
+            {
+                brushForLoading = brushForManipulation;
+            }
+            
+        
+            var count = 0;
+            for (int i = 0; i < brushForLoading.width; i++)
+            {
+                for (int j = 0; j < brushForLoading.height; j++)
+                { 
+                    Color pixel = brushForLoading.GetPixel(j, i);
+                    // if it's a white color then just debug...
+                    if (pixel == Color.black)
+                    {
+                        count+=1;
+                    }
+                }
+            }
+            loadedBrush = new brushPixel[count];
+            computedBrush = new brushPixel[count];
+            count = 0;
+            for (int i = 0; i < brushForLoading.width; i++)
+            {
+                for (int j = 0; j < brushForLoading.height; j++)
+                { 
+                    Color pixel = brushForLoading.GetPixel(j, i);
+                    // if it's a white color then just debug...
+                    if (pixel == Color.black)
+                    {
+                        loadedBrush[count].xPos = i- (int)Mathf.Round(brushForLoading.width/2);
+                        loadedBrush[count].yPos = j- (int)Mathf.Round(brushForLoading.height/2);
+                        loadedBrush[count].pixelBrushStrength = realBrushStrenght;
+                        count+=1;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Brush Not Found - implement real error handling function");
+        }
+    }
+
+    /// <summary>
+    /// Scales the loaded brush. 
+    /// </summary>
+    private void scaleBrush()
+    {
+        int targetWidth = (int)Mathf.Round(originalBrush.width*brushSize);
+        int targetHeight = (int)Mathf.Round(originalBrush.height*brushSize);
+        Texture2D result=new Texture2D((int)Mathf.Round(originalBrush.width*brushSize),targetHeight,originalBrush.format,false);
+        float incX=(1.0f / (float)targetWidth);
+        float incY=(1.0f / (float)targetHeight);
+        for (int i = 0; i < result.height; ++i) {
+         for (int j = 0; j < result.width; ++j) {
+             Color newColor = originalBrush.GetPixelBilinear((float)j / (float)result.width, (float)i / (float)result.height);
+             result.SetPixel(j, i, newColor);
+         }
+     }
+     result.Apply();
+     brushForManipulation = result;
+    }
+}
